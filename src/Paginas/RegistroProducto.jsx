32bf21a000c2a56b311b7d33ca44235062fa1071
CRYKER7 <78180693+CRYKER7@users.noreply.github.com>
2021-11-23 22:42:09 -0600

    formulario

--- src/Paginas/RegistroProducto.jsx
+++ src/Paginas/RegistroProducto.jsx
@@ -10,14 +10,53 @@ const RegistroProducto = () => {
             <section className="page-section row overflow-hidden ms-0 me-0">
                 <div className="col-12 row bg-faded text-center p-5 m-0">
                     <h3>Registro de productos</h3>
-                    <form className="form-horizontal">
-                        <div class="form-group required row">
-                            <label for="subId" class="control-label col-md-4 requiredField">Sub ID<span class="asteriskField">*</span> </label>
+                    <form className="form-horizontal row">
+
+                        <div class="form-group required row mb-3">
+                            <label for="categoria" class="control-label col-md-4 requiredField text-start">Categoria<span class="asteriskField">*</span> </label>
                             <div class="controls col-md-8 ">
-                                <input class="input-md  textinput textInput form-control" id="id_username" maxlength="30" name="username" placeholder="Choose your username" type="text" />
+                                <input class="input-md  textinput textInput form-control" id="categoria" maxlength="20" name="categoria" placeholder="Ingrese la categoria del producto" type="text" />
                             </div>
                         </div>
+
+                        <div class="form-group required row mb-3">
+                            <label for="subId" class="control-label col-md-4 requiredField text-start">Sub ID<span class="asteriskField">*</span></label>
+                            <div class="controls col-md-8 ">
+                                <input class="input-md textinput textInput form-control" id="subId" maxlength="3" name="subid" placeholder="Ingrese sub id del producto" type="text" />
+                            </div>
+                        </div>
+
+                        <div class="form-group required row mb-3">
+                            <label for="nombre" class="control-label col-md-4 requiredField text-start">Nombre<span class="asteriskField">*</span> </label>
+                            <div class="controls col-md-8 ">
+                                <input class="input-md  textinput textInput form-control" id="nombre" maxlength="30" name="nombre" placeholder="Ingrese el nombre del producto" type="text" />
+                            </div>
+                        </div>
+
+                        <div class="form-group required row mb-3">
+                            <label for="descripcion" class="control-label col-md-4 requiredField text-start">Descripcion del producto<span class="asteriskField">*</span> </label>
+                            <div class="controls col-md-8 ">
+                                <input class="input-md  textinput textInput form-control" id="descripcion" maxlength="100" name="descripcion" placeholder="Ingrese la descripcion del producto" type="text" />
+                            </div>
+                        </div>
+
+                        <div class="form-group required row mb-3">
+                            <label for="precio" class="control-label col-md-4 requiredField text-start">Precio del producto<span class="asteriskField">*</span> </label>
+                            <div class="controls col-md-8 ">
+                                <input class="input-md  textinput textInput form-control" id="precio" maxlength="10" name="precio" placeholder="Ingrese el precio del producto" type="text" />
+                            </div>
+                        </div>
+
+                        <div class="form-group required row mb-3 col-2">
+                            <button class="btn btn-info" type="submit">GUARDAR</button>
+                        </div>
+
                     </form>
+
+                    <div class="text-center col-8">
+                            <h6>El estaus del producto se generara automaticamente como inactivo debera modificarlo y confirmar los datos para validar su estatus activo y ser visuble al publico</h6>
+                    </div>
+
                 </div>
             </section>
                 
